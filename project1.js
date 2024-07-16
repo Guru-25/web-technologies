ff9ea9caf77725a394e8b2a6c03eac8a4bdeb1f4
Guru <guru@duck.com>
2024-07-16 15:06:25 +0530

    first commit

---
+++ project1.js
@@ -0,0 +1,8 @@
+function showTab(tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "block";
+}
